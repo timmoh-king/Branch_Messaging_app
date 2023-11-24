135f1122f8c8bfa6d381f6ad1aed9ebdb0c90b05
timmoh-king <timothykariuki43@gmail.com>
2023-11-24 12:48:08 +0300

    import and set up the socket.io connection on the index.js file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-
+const { Server } = require('socket.io');
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ['GET', 'POST'],
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('disconnect', () => {
+        console.log("user disconnected", socket.id);
+    });
+});
 
 const port = 3005
 server.listen(port, () => {
