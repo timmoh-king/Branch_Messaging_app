9bd538a74c37ca1f50ab9b55fd44f334531578e1
timmoh-king <timothykariuki43@gmail.com>
2023-11-24 12:38:16 +0300

    setup the index.js (main_file) and update the start script on package.json file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
 
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const port = 3005
+server.listen(port, () => {
+    console.log(`SERVER RUNNING ON PORT ${port}`);
+});
