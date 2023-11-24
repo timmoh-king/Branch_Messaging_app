9789a6eb80ab6768efba6e41a1b62c1f957ddbd7
timmoh-king <timothykariuki43@gmail.com>
2023-11-24 14:34:03 +0300

    install and connect socket.io-client on both client and server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io.connect("http://localhost:3005");
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [chat, setChat] = useState('');
+
+  const joinChat = () => {
+    if (username !== '' && chat !== '') {
+      socket.emit("join_chat", chat);
+    }
+  }
+
   return (
     <div className="App">
+      <h3>Join the chat</h3>
+      <input type='text' placeholder='Enter username...' onChange={(e) => {setUsername(e.target.value)}} />
+      <input type='text' placeholder='Chat ID...' onChange={(e) => {setChat(e.target.value)}} />
+      <button onClick={joinChat}>Join the chat</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -16,10 +16,15 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
+    console.log(`User connected ${socket.id}`);
+
+    socket.on("join_chat", (data) => {
+        socket.join(data);
+        console.log(`User ${socket.id} joined chat ${data}`);
+    })
 
     socket.on('disconnect', () => {
-        console.log("user disconnected", socket.id);
+        console.log("User disconnected", socket.id);
     });
 });
 
